3bed1e3a20e96d4645a0343d39bb1ec795810d2d
Harry Culpan <harry@culpan.org>
2017-03-08 09:27:58 -0500

    Updating votes

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,6 @@ cd _site
 zip -r9 site.zip *
 echo "Copying to charnomic.com..."
 scp site.zip root@charnomic.com:/root
-#scp rdeploy.sh root@culpan.net:/root
 
 echo "Excuting deploy on charnomic.com"
 ssh root@charnomic.com 'bash -s' < ../rdeploy.sh
